1baab2bd3cf4e33cd7bf16c2908be57d3141b040
garyCoffey <garycoffey12@gmail.com>
2018-10-30 18:01:52 -0500

    initial project setup commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Insert cool statement!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
