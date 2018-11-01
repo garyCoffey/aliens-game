d8a134b41a7015bc7106c0d8dfb8ed2db4b76d8f
garyCoffey <garycoffey12@gmail.com>
2018-11-01 11:52:49 -0500

    Added reducres/containers and some reudx to the main index

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Insert cool statement!</h1>
+        <h1>{this.props.message}</h1>
       </div>
     );
   }
 }
+
+App.PropTypes = {
+  messagge: PropTypes.string.isRequired
+};
+
 
 export default App;
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+const initialState = {
+  message: 'adding redux is easy'
+};
+
+function reducer(state = initialState) {
+  return state;
+}
+
+export default reducer;
