b00c4cbcd852e8c27085ecb74c3210a549f6bcb1
garyCoffey <garycoffey12@gmail.com>
2018-11-01 15:07:28 -0500

    Cannon now rotates with the mouse

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,24 +1,30 @@
 import React from 'react';
-import Sky from './Sky'
-import Ground from './Ground'
-import CannonBase from './CannonBase'
-import CannonPipe from './CannonPipe'
+import PropTypes from 'prop-types';
+import Sky from './Sky';
+import Ground from './Ground';
+import CannonBase from './CannonBase';
+import CannonPipe from './CannonPipe';
 
-const Canvas = () => {
+const Canvas = (props) => {
   const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
-
   return (
     <svg
-      id="aliens-canvas"
+      id="aliens-go-home-canvas"
       preserveAspectRatio="xMaxYMax none"
+      onMouseMove={props.trackMouse}
       viewBox={viewBox}
     >
       <Sky />
       <Ground />
-      <CannonPipe rotation={45} />
-      <CannonBase />cx={0} cy={0} r={50} />
+      <CannonPipe rotation={props.angle} />
+      <CannonBase />
     </svg>
   );
 };
 
-export default Canvas
+Canvas.propTypes = {
+  angle: PropTypes.number.isRequired,
+  trackMouse: PropTypes.func.isRequired,
+};
+
+export default Canvas;
